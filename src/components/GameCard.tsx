4fd9f47608f914de16bbe5110dfd4ad029528b4b
Krystian <krchr@wp.pl>
2023-12-04 13:08:50 +0100

    Change a few small things

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,4 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
 import getCroppedImageUrl from '../services/image-url'
 import CriticScore from './CriticScore'
@@ -7,23 +6,25 @@ import PlatformIconList from './PlatformIconList'
 import Emoji from './Emoji'
 
 interface Props {
-  game: Game
+	game: Game
 }
 
 const GameCard = ({ game }: Props) => {
-  return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent='space-between' mb={3}>
-          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top}></Emoji></Heading>
-
-      </CardBody>
-    </Card>
-  )
+	return (
+		<Card _hover={{ transform: 'scale(0.95)', filter: 'brightness(110%)' }} transition="transform 0.3s, filter 0.3s">
+			<Image src={getCroppedImageUrl(game.background_image)} />
+			<CardBody>
+				<HStack justifyContent="space-between" mb={3}>
+					<PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+					<CriticScore score={game.metacritic} />
+				</HStack>
+				<Heading fontSize="2xl">
+					{game.name}
+					<Emoji rating={game.rating_top}></Emoji>
+				</Heading>
+			</CardBody>
+		</Card>
+	)
 }
 
 export default GameCard
